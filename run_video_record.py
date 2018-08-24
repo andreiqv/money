b8910530d9f60999f86e289c5e118a442b466590
z <kukharev.av@gmail.com>
2018-08-24 14:08:55 +0300

    1

--- run_video_record.py
+++ run_video_record.py
@@ -1,6 +1,8 @@
 import os
+import sys
+import argparse
 
-def run_record(in_dir):
+def run_record(in_dir, i):
 	
 	#datafile = 'data/money_nonb.data'
 
@@ -19,16 +21,32 @@ def run_record(in_dir):
 		#cmd = './darknet classifier valid {0} cfg/{1}.cfg {2} > {3}'\
 		#	.format(datafile, network_name, weights_filepath, tmpfile)
 
-		cmd = "./darknet detector demo data/money.data cfg/yolov3-money.cfg"\
-			 " ../yolov3-money_40000.weights {0} -thresh 0.1 -i 1 -prefix {1}"\
-			 .format(in_file, out_file)
+		cmd = "..//darknet detector demo ../money.data ../yolov3-money.cfg"\
+			 " ../yolov3-money_40000.weights {0} -thresh 0.1 -i {0} -prefix {1}"\
+			 .format(in_file, out_file, i)
 
 		print(cmd)
 		os.system(cmd)
 
 
+#---
+def createParser ():
+	"""
+	ArgumentParser
+	"""
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-i', '--i', default=1, type=int,\
+		help='gpu')
+	#parser.add_argument('-phi', '--phi', dest='phi', action='store_true')
+	return parser
+
 
 if __name__ == '__main__':
 
-	in_dir = '/home/chichivica/Data/Datasets/Money/raw_video/1/'
-	run_record(in_dir)
+	parser = createParser()
+	arguments = parser.parse_args(sys.argv[1:])			
+	print('set arguments.i =', arguments.i)
+
+	i = arguments.i
+	in_dir = '/home/chichivica/Data/Datasets/Money/raw_video/{0}'.format(i)
+	run_record(in_dir, i)
